820fbae9400daa4290a35a1e2e1d9d2bfbe4a60b
sbkat <kathydangg@gmail.com>
2019-12-31 12:08:00 -0800

    added apiand form

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ import React from 'react';
 import Titles from './components/Titles';
 import Form from './components/Form';
 import Weather from './components/Weather';
+
+const API_KEY = '9521166e48e8976c994c638e8f5de239';
 class App extends React.Component {
+  getWeather = async() => {
+    // arrow allows to use this keyword in place on constructor before calling it
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=${API_KEY}&units=metric
+    `);
+    const data = await api_call.json();
+
+  }
   render() {
     return (
       <div>
