a9a552d71b1d71c34cf4e439321271edee6951ec
sbkat <kathydangg@gmail.com>
2020-01-02 16:19:14 -0800

    converted class to stateless functional components

--- src/App.js
+++ src/App.js
@@ -5,20 +5,54 @@ import Weather from './components/Weather';
 
 const API_KEY = '9521166e48e8976c994c638e8f5de239';
 class App extends React.Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
   getWeather = async(e) => {
     e.preventDefault();
-    // arrow allows to use this keyword in place on constructor before calling it
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=${API_KEY}&units=metric
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}&units=metric
     `);
     const data = await api_call.json();
-    console.log(data);
+    if(city && country) {
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter the value."
+      });
+    }
   }
   render() {
     return (
       <div>
         <Titles />
         <Form getWeather={this.getWeather} />
-        <Weather />
+        <Weather 
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+        />
       </div>
     );
   }
